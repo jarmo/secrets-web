c0324fe1fcbff2b6a7c1055dd66d56f113e46cd0
Jarmo Pertman <jarmo.p@gmail.com>
2019-09-09 13:03:39 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/jarmo/secrets-web
+
+go 1.12
+
+require (
+	github.com/gin-contrib/sessions v0.0.1
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/jarmo/secrets/v5 v5.0.0
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/ugorji/go v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
+	golang.org/x/sys v0.0.0-20190826190057-c7b8b68b1456 // indirect
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/memstore"
+	"github.com/gin-gonic/gin"
+	"github.com/jarmo/secrets/v5/storage"
+	"github.com/jarmo/secrets/v5/storage/path"
+	"github.com/jarmo/secrets/v5/vault"
+)
+
+func redirect(c *gin.Context, path string) {
+	c.Redirect(http.StatusFound, path)
+	c.AbortWithStatus(http.StatusFound)
+}
+
+func authenticated() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+
+		if session.Get("vaultPath") == nil {
+			redirect(c, "/login")
+		}
+	}
+}
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	sessionSecret := os.Getenv("SECRETS_SESSION_SECRET")
+	if sessionSecret == "" {
+		fmt.Fprintln(os.Stderr, "SECRETS_SESSION_SECRET environment variable not set!")
+		os.Exit(1)
+	}
+	memstore := memstore.NewStore([]byte(sessionSecret))
+
+	router.Use(sessions.Sessions("secrets", memstore))
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/login", func(c *gin.Context) {
+		session := sessions.Default(c)
+		if session.Get("vaultPath") != nil {
+			redirect(c, "/")
+		} else {
+			c.HTML(http.StatusOK, "login.tmpl", gin.H{})
+		}
+	})
+
+	router.POST("/login", func(c *gin.Context) {
+		vaultAlias := c.PostForm("user")
+		password := c.PostForm("password")
+
+		if path, aliasErr := path.Get(vaultAlias); aliasErr != nil {
+			c.HTML(http.StatusOK, "login.tmpl", gin.H{
+				"error": aliasErr,
+				"user":  vaultAlias,
+			})
+		} else {
+			if _, vaultErr := storage.Read(path, []byte(password)); vaultErr != nil {
+				c.HTML(http.StatusOK, "login.tmpl", gin.H{
+					"error": vaultErr,
+					"user":  vaultAlias,
+				})
+			} else {
+				session := sessions.Default(c)
+				session.Set("vaultPath", path)
+				session.Set("password", password)
+				session.Save()
+				redirect(c, "/")
+			}
+		}
+	})
+
+	protected := router.Group("", authenticated())
+
+	protected.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	})
+
+	protected.POST("/logout", func(c *gin.Context) {
+		session := sessions.Default(c)
+		session.Clear()
+		session.Save()
+		redirect(c, "/login")
+	})
+
+	protected.POST("/", func(c *gin.Context) {
+		filter := c.PostForm("filter")
+		session := sessions.Default(c)
+		secrets, readErr := storage.Read(session.Get("vaultPath").(string), []byte(session.Get("password").(string)))
+		result := vault.List(secrets, filter)
+
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"error":   readErr,
+			"filter":  filter,
+			"secrets": result,
+		})
+	})
+
+	return router
+}
+
+func main() {
+	r := setupRouter()
+	r.Run("localhost:8080")
+}
