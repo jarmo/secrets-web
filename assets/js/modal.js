52c94be0e2cbcca0482ab093745aa6bd19d58605
Jarmo Pertman <jarmo.p@gmail.com>
2019-11-05 11:23:47 +0200

    Handle modals with JavaScript since we want more functionality with them

---
+++ assets/js/modal.js
@@ -0,0 +1,53 @@
+function Modal() {
+  document.addEventListener('click', showModal)
+  document.addEventListener('click', closeOnClick)
+  document.addEventListener('click', closeOnClickOutside)
+  document.addEventListener('keyup', closeOnEscapeKeypress)
+
+  function showModal(event) {
+    var target = event.target
+    if (target.classList.contains("btn-modal")) {
+      setTimeout(function() {
+        show(document.querySelector(target.dataset.target))
+      }, 0)
+    }
+  }
+
+  function closeOnClick(event) {
+    var target = event.target
+    if (target.classList.contains("close")) {
+      event.preventDefault()
+      close(shownModal())
+    }
+  }
+
+  function closeOnClickOutside(event) {
+    if (!event.target.closest(".modal-container")) {
+      var modal = shownModal()
+      if (modal) close(modal)
+    }
+  }
+
+  function closeOnEscapeKeypress(event) {
+    if (event.keyCode == 27) {
+      var modal = shownModal()
+      if (modal) close(modal)
+    }
+  }
+
+  function shownModal() {
+    return Array.from(document.querySelectorAll(".modal")).filter(isShown)[0]
+  }
+
+  function isShown(modal) {
+    return modal.style.display !== "none"
+  }
+
+  function show(modal) {
+    modal.style.display = "block"
+  }
+
+  function close(modal) {
+    modal.style.display = "none"
+  }
+}
